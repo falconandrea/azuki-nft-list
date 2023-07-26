e8e3e1760787fdc8299919908462767656f6d2cf
Andrea Falcon <83451204+falconandrea@users.noreply.github.com>
2023-07-26 12:55:05 +0200

    feat: grid and cards with nfts

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,34 +1,22 @@
-export function Card() {
+import Image from 'next/image'
+
+export function Card({nft}) {
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
         <article className="overflow-hidden rounded-lg shadow-lg">
-            <a href="#">
-                <img alt="Placeholder" className="block h-auto w-full" src="https://picsum.photos/600/400/?random" />
+            <a href={nft.link} target='_blank'>
+              {nft.image && (
+                <Image className="block h-auto w-full" alt={`#${nft.id}`} loading="lazy" width={150} height={150} src={nft.image} />
+              )}
             </a>
 
-            <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+            <div className="flex items-center justify-between leading-tight p-2 md:p-4">
                 <h1 className="text-lg">
-                    <a className="no-underline hover:underline text-black" href="#">
-                        Article Title
+                    <a className="no-underline hover:underline text-black" href={nft.link} target='_blank'>
+                        Azuki #{nft.id}
                     </a>
                 </h1>
-                <p className="text-grey-darker text-sm">
-                    11/1/19
-                </p>
-            </header>
-
-            <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-                <a className="flex items-center no-underline hover:underline text-black" href="#">
-                    <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/32/32/?random" />
-                    <p className="ml-2 text-sm">
-                        Author Name
-                    </p>
-                </a>
-                <a className="no-underline text-grey-darker hover:text-red-dark" href="#">
-                    <span className="hidden">Like</span>
-                    <i className="fa fa-heart"></i>
-                </a>
-            </footer>
+            </div>
         </article>
     </div>
   )
--- frontend/components/Grid.js
+++ frontend/components/Grid.js
@@ -1,19 +1,91 @@
+import React, { useEffect, useState } from 'react'
 import { Card } from "./Card"
+import { Loading } from './Loading'
+
+//import from the generated directory
+const graphClient =  require('./../.graphclient')
+
+const convertIPFSLink = (ipfs) => {
+  return ipfs.replace("ipfs://", "https://ipfs.io/ipfs/");
+}
+
+const getOpenseaLink = (id) => {
+  return 'https://opensea.io/assets/ethereum/0xed5af388653567af2f388e6224dc7c4b3241c544/' + id
+}
+
+async function getNFTs(){
+  // Query to fetch the nfts data
+  const nftsQuery = `
+  query ListNFTs($skip: Int = 0) {
+    properties(first: 1000, skip: $skip) {
+      id
+      image
+      type
+      hair
+      headgear
+      face
+      clothing
+      eyes
+      mouth
+      background
+      ear
+      offhand
+      special
+      neck
+    }
+  }`
+
+  // Loop to get all results
+  const allResults = []
+  let skip = 0
+  let hasItems = true
+  do {
+    const result = await graphClient.execute(nftsQuery, { skip })
+    allResults.push(...result.data.properties)
+    skip += 1000
+    hasItems = result.data.properties.length > 0
+  } while (hasItems)
+
+  return allResults
+}
+
+async function getData(){
+  let data = await getNFTs()
+
+  // Order array by id field and return
+  data = data.map((item) => ({
+    ...item,
+    id: parseInt(item.id),
+    image: convertIPFSLink(item.image),
+    link: getOpenseaLink(item.id)
+  })).sort((a, b) => a.id - b.id)
+
+  return data
+}
 
 export function Grid() {
+  const [nfts, setNfts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    getData().then((data) => {
+      setNfts(data)
+      console.log(data[0])
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <div id="grid">
+      {loading && (
+        <Loading />
+      )}
       <div className="container my-12 mx-auto px-4 md:px-12">
         <div className="flex flex-wrap -mx-1 lg:-mx-4">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {nfts.map((nft) => (
+            <Card key={nft.id} nft={nft} />
+          ))}
         </div>
       </div>
     </div>
